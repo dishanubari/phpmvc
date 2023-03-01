f54c1d0515b76e614439d37f77853f544d57a1a7
dishanubari <dishanubaripramudia07@gmail.com>
2023-03-01 10:27:19 +0700

    update latihan view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,22 @@
 <?php
 
-class About {
-    public function index($nama = 'Dishanubari', $pekerjaan = 'Dosen')
+class About extends Controller {
+    public function index($nama = 'Dishanubari', $pekerjaan = 'Dosen', $umur = 22)
     {
-        echo "Halo nama saya $nama, saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header');
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
+
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Page';
+        $this->view('templates/header');
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
