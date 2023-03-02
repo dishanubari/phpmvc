c3816199d71dcb82a75b2cf486d86d4e0724aa4c
dishanubari <dishanubaripramudia07@gmail.com>
2023-03-02 15:55:24 +0700

    latihan assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,7 @@
- <h1>About me</h1>
+ <div class="container">
+ 
+ <h1 class="mt-4">About me</h1>
+   <img src="<?= BASEURL; ?>/img/disha.png" alt="Dishanubari Pramudia" width="200" class="rounded-circle">
     <p>Halo, nama saya <?= $data['nama']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+    
+</div>
