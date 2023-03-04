f23b85dc83a88e6c91b72c0a5dac4207e19589b4
dishanubari <dishanubaripramudia07@gmail.com>
2023-03-04 19:33:25 +0700

    update latihan model

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,29 +1,25 @@
 <?php
 
 class Mahasiswa_model {
-    private $mhs = [
-        [
-            "nama" => "Dishanubari Pramudia",
-            "nim" => "41037006191049",
-            "email" => "dishanubaripramudiaa@gmail.com",
-            "jurusan"  => "Teknik Informatika"
-        ],
-        [
-            "nama" => "Nagita Syifa Aulia",
-            "nim" => "41037006191050",
-            "email" => "nagitasyifaa@gmail.com",
-            "jurusan"  => "Teknik Arsitektur"
-        ],
-        [
-            "nama" => "Dirgantara Pramudia",
-            "nim" => "41037006191051",
-            "email" => "dirgaap@gmail.com",
-            "jurusan"  => "Teknik Mesin"
-        ]
-     ];
+    private $dbh; //database handler
+    private $stmt;
+
+    public function __construct()
+    {
+        // data source name
+        $dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
 
     public function getAllMahasiswa()
     {
-        return $this->mhs;
+       $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+       $this->stmt->execute();
+       return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
